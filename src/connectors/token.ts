e740da4e00db153c6e64813c6f23f98b053f185e
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-05-14 23:54:59 +0200

    Added token creation for token connector

--- src/connectors/token.ts
+++ src/connectors/token.ts
@@ -1,10 +1,17 @@
+import { createSigner } from "fast-jwt";
 import type { ConnectorProtocol } from "./connectors.d.ts";
+
+/**
+ * 2 minutes earlier. This should let us avoid
+ * any kind of error from expired token from the server.
+ */
+const TOKEN_VALIDITY_TIME_1H = 58 * 60 * 1000;
 
 export interface TokenConnectorData {
 	/**
-	 * PKCS 8 file path to be read and used as the private key.
+	 * PKCS 8 file content to be read and used as the private key.
 	 */
-	key: string;
+	key: Uint8Array;
 
 	/**
 	 * A 10-character string Key ID provided by Apple.
@@ -19,7 +26,7 @@ export interface TokenConnectorData {
 
 interface TokenMemory {
 	token: string;
-	creationTimestamp: number;
+	issuedAt: number;
 }
 
 /**
@@ -30,12 +37,52 @@ interface TokenMemory {
  */
 
 export function TokenConnector(details: TokenConnectorData): ConnectorProtocol {
-	const tokenMemory: TokenMemory = {
-		token: "",
-		creationTimestamp: 0,
-	};
+	if (!details.key || ArrayBuffer.isView(details.key)) {
+		throw new Error("Token connector field 'key' is missing or is not an Uint8Array.");
+	}
+
+	if (!details.keyId) {
+		throw new Error("Token connector field 'keyId' is missing.");
+	}
+
+	if (!details.teamIdentifier) {
+		throw new Error("Token connector field 'teamIdentifier' is missing.");
+	}
+
+	let tokenMemory: TokenMemory | undefined = undefined;
 
 	return {
-		async send(notification, target) {},
+		async send(notification, target) {
+			if (!tokenMemory || isTokenExpired(tokenMemory)) {
+				tokenMemory = createToken(details);
+			}
+
+			const { token } = tokenMemory;
+
+			/**
+			 * @TODO implement sending the notification
+			 */
+		},
+	};
+}
+
+function isTokenExpired(tokenMemory: TokenMemory): boolean {
+	return tokenMemory.issuedAt + TOKEN_VALIDITY_TIME_1H >= Date.now();
+}
+
+function createToken(details: TokenConnectorData): TokenMemory {
+	const signer = createSigner({
+		algorithm: "ES256",
+		kid: details.keyId,
+		iss: details.teamIdentifier,
+	});
+
+	const token = signer({
+		iss: details.teamIdentifier,
+	});
+
+	return {
+		token,
+		issuedAt: Date.now(),
 	};
 }
