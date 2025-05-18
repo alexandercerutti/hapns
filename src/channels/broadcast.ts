47fa4ffbc0d21c9b7d2be0d9bfa57f8d13189e86
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-05-18 12:37:05 +0200

    Added functions to create, read, read all and delete channels

---
+++ src/channels/broadcast.ts
@@ -0,0 +1,184 @@
+import type { ConnectorProtocol } from "../connectors/connectors.d.ts";
+import { BroadcastChannel } from "../targets/BroadcastChannel.js";
+
+interface BroadcastChannelSettings {
+	bundleId: string;
+	messageStoragePolicy?: 1 | 0;
+	apnsRequestId?: string;
+	useSandbox?: boolean;
+}
+
+const BROADCAST_SANDBOX_BASE_URL = "https://api-manage-broadcast.sandbox.push.apple.com:2195";
+const BROADCAST_PRODUCTION_BASE_URL = "https://api-manage-broadcast.push.apple.com:2196";
+
+export async function createBroadcastChannel(
+	connector: ConnectorProtocol,
+	settings: BroadcastChannelSettings,
+	useSandbox: boolean = false,
+): Promise<BroadcastChannel> {
+	if (!connector || typeof connector.send !== "function") {
+		throw new Error("Connector is missing or is not a valid connector.");
+	}
+
+	if (!settings || typeof settings !== "object") {
+		throw new Error("Settings are missing or are not a valid settings object.");
+	}
+
+	if (!settings.bundleId || typeof settings.bundleId !== "string") {
+		throw new Error("Settings bundleId is missing or is not a string.");
+	}
+
+	const baseUrl = useSandbox ? BROADCAST_SANDBOX_BASE_URL : BROADCAST_PRODUCTION_BASE_URL;
+
+	const channelHeaders = {
+		"apns-request-id": settings.apnsRequestId || "",
+	};
+
+	const channelCreationResponse = await connector.send({
+		method: "POST",
+		baseUrl,
+		requestPath: `/1/apps/${settings.bundleId}/channels`,
+		headers: channelHeaders,
+		body: {
+			messageStoragePolicy: settings.messageStoragePolicy,
+			"push-type": "LiveActivity",
+		},
+	});
+
+	const { headers } = channelCreationResponse;
+
+	const { "apns-channel-id": channelId } = headers as Record<string, string>;
+
+	return BroadcastChannel(channelId, settings.bundleId);
+}
+
+interface ChannelReadResponseBody {
+	messageStoragePolicy: number;
+	pushType: string;
+}
+
+export async function readChannel(
+	connector: ConnectorProtocol,
+	channelId: string,
+	bundleId: string,
+	useSandbox: boolean = false,
+): Promise<ChannelReadResponseBody> {
+	if (!connector || typeof connector.send !== "function") {
+		throw new Error("Connector is missing or is not a valid connector.");
+	}
+
+	if (!channelId || typeof channelId !== "string") {
+		throw new Error("Channel ID is missing or is not a string.");
+	}
+
+	if (!bundleId || typeof bundleId !== "string") {
+		throw new Error("Bundle ID is missing or is not a string.");
+	}
+
+	const baseUrl = useSandbox ? BROADCAST_SANDBOX_BASE_URL : BROADCAST_PRODUCTION_BASE_URL;
+
+	const channelHeaders = {
+		"apns-channel-id": channelId || "",
+	};
+
+	const channelReadResponse = await connector.send({
+		method: "GET",
+		baseUrl,
+		requestPath: `/1/apps/${bundleId}/channels`,
+		headers: channelHeaders,
+		body: {},
+	});
+
+	const { messageStoragePolicy, pushType } =
+		(await channelReadResponse.body.json()) as ChannelReadResponseBody;
+
+	return {
+		messageStoragePolicy,
+		pushType,
+	};
+}
+
+/**
+ * Sends a request to delete an existing broadcast channel.
+ *
+ * @param connector
+ * @param channelId
+ * @param bundleId
+ */
+export async function deleteChannel(
+	connector: ConnectorProtocol,
+	channelId: string,
+	bundleId: string,
+	useSandbox: boolean = false,
+): Promise<{ success: true; apnsRequestId: string }> {
+	if (!connector || typeof connector.send !== "function") {
+		throw new Error("Connector is missing or is not a valid connector.");
+	}
+
+	if (!channelId || typeof channelId !== "string") {
+		throw new Error("Channel ID is missing or is not a string.");
+	}
+
+	if (!bundleId || typeof bundleId !== "string") {
+		throw new Error("Bundle ID is missing or is not a string.");
+	}
+
+	const baseUrl = useSandbox ? BROADCAST_SANDBOX_BASE_URL : BROADCAST_PRODUCTION_BASE_URL;
+
+	const channelDeleteResponse = await connector.send({
+		method: "DELETE",
+		baseUrl,
+		requestPath: `/1/apps/${channelId}/channels`,
+		headers: {
+			"apns-channel-id": channelId,
+		},
+		body: {},
+	});
+
+	const { "apns-request-id": apnsRequestId } = channelDeleteResponse.headers as Record<
+		string,
+		string
+	>;
+
+	return {
+		success: true,
+		apnsRequestId,
+	};
+}
+
+/**
+ * Reads all channels for a given bundle ID.
+ * Creates an array of valid targets against which notifications can be sent.
+ *
+ * @param connector
+ * @param bundleId
+ * @param useSandbox
+ * @returns
+ */
+export async function readAllChannels(
+	connector: ConnectorProtocol,
+	bundleId: string,
+	useSandbox: boolean = false,
+): Promise<BroadcastChannel[]> {
+	if (!connector || typeof connector.send !== "function") {
+		throw new Error("Connector is missing or is not a valid connector.");
+	}
+
+	if (!bundleId || typeof bundleId !== "string") {
+		throw new Error("Bundle ID is missing or is not a string.");
+	}
+
+	const baseUrl = useSandbox ? BROADCAST_SANDBOX_BASE_URL : BROADCAST_PRODUCTION_BASE_URL;
+
+	const channelReadResponse = await connector.send({
+		method: "GET",
+		baseUrl,
+		requestPath: `/1/apps/${bundleId}/all-channels`,
+		headers: {},
+		body: {},
+	});
+
+	const { channels } = (await channelReadResponse.body.json()) as { channels: string[] };
+
+	return channels.map((channelId) => BroadcastChannel(channelId, bundleId));
+}
--- src/targets/BroadcastChannel.ts
+++ src/targets/BroadcastChannel.ts
@@ -2,12 +2,21 @@ import type { NotificationTarget } from "./target.js";
 
 const BROADCAST_DELIVERY_PATH = "/4/device/";
 
-export function BroadcastChannel(channelId: string, bundleId: string): NotificationTarget {
+export interface BroadcastChannel extends NotificationTarget {
+	channelId: string;
+}
+
+export function BroadcastChannel(channelId: string, bundleId: string): BroadcastChannel {
 	if (typeof channelId !== "string") {
 		throw new Error("Broadcast channel id not provided.");
 	}
 
+	if (!bundleId || typeof bundleId !== "string") {
+		throw new Error("Bundle ID is missing or is not a string.");
+	}
+
 	return {
+		channelId,
 		getBaseUrl(useSandbox: boolean = false) {
 			return useSandbox ? "https://api.sandbox.push.apple.com" : "https://api.push.apple.com";
 		},
