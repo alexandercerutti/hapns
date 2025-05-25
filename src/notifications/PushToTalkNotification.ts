40dc555b495bcf8f46b57b1164c9943a607678b8
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-05-25 23:28:38 +0200

    Added structure for PushToTalkNotification

--- src/notifications/PushToTalkNotification.ts
+++ src/notifications/PushToTalkNotification.ts
@@ -9,4 +9,37 @@ export interface NotificationCustomData {}
 type NotificationData = NotificationHeaders &
 	NotificationBody<Record<string, string>, NotificationCustomData>;
 
-export function PushToTalkNotification(topic: string, data: NotificationData): Notification<Record<string, string> {}
+/**
+ * Create a Push-to-Talk notification.
+ *
+ * @param topic The topic of the notification. It will be suffixed, if needed, with `.voip-ptt`.
+ * @param data
+ * @returns
+ */
+export function PushToTalkNotification(
+	topic: string,
+	data: NotificationData,
+): Notification<Record<string, string>> {
+	const { expiration = 0, collapseID, priority = 10 } = data;
+
+	return {
+		pushType: "pushtotalk",
+		get topic() {
+			if (typeof topic !== "string") {
+				throw new TypeError("Topic must be a string");
+			}
+
+			if (topic.endsWith(".voip-ptt")) {
+				return topic;
+			}
+
+			return `${topic}.voip-ptt`;
+		},
+		body: {
+			aps: {},
+		},
+		expiration,
+		collapseID,
+		priority,
+	};
+}
