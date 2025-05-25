b0f3006003910cb8e9b553b20eb406a2634d8c7d
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-05-25 22:16:56 +0200

    Added structure for LocationNotification

--- src/notifications/LocationNotification.ts
+++ src/notifications/LocationNotification.ts
@@ -1,4 +1,4 @@
-import type { NotificationDetails } from "./notification.js";
+import type { Notification, NotificationDetails } from "./notification.js";
 /**
  * Empty interface on purpose to allow for TS
  * autocompletion to be extended by the user.
@@ -9,4 +9,29 @@ export interface NotificationCustomData {}
 export function LocationNotification(
 	topic: string,
 	data: NotificationDetails<Record<string, string>, NotificationCustomData>,
-): void {}
+): Notification<Record<string, string>, NotificationCustomData> {
+	const { expiration = 0, collapseID, priority = 10 } = data;
+
+	return {
+		pushType: "location",
+		get topic() {
+			if (typeof topic !== "string") {
+				throw new TypeError("Topic must be a string");
+			}
+
+			if (topic.endsWith(".location-query")) {
+				return topic;
+			}
+
+			return `${topic}.location-query`;
+		},
+		get body() {
+			return {
+				aps: {},
+			};
+		},
+		expiration,
+		collapseID,
+		priority,
+	};
+}
