95b7f0bb4b85d640594091df58766005c6becfa6
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-05-25 23:31:33 +0200

    Added structure for VoipNotification

--- src/notifications/VoipNotification.ts
+++ src/notifications/VoipNotification.ts
@@ -9,7 +9,37 @@ export interface NotificationCustomData {}
 type NotificationData = NotificationHeaders &
 	NotificationBody<Record<string, string>, NotificationCustomData>;
 
+/**
+ * Create a notification about an incoming VoIP call through PushKit.
+ *
+ * @param topic The topic of the notification. It will be suffixed, if needed, with `.voip`.
+ * @param data
+ * @returns
+ */
 export function VoipNotification(
 	topic: string,
 	data: NotificationData,
-): Notification<Record<string, string>> {}
+): Notification<Record<string, string>> {
+	const { expiration = 0, collapseID, priority = 10 } = data;
+
+	return {
+		pushType: "voip",
+		get topic() {
+			if (typeof topic !== "string") {
+				throw new TypeError("Topic must be a string");
+			}
+
+			if (topic.endsWith(".voip")) {
+				return topic;
+			}
+
+			return `${topic}.voip`;
+		},
+		body: {
+			aps: {},
+		},
+		expiration,
+		collapseID,
+		priority,
+	};
+}
