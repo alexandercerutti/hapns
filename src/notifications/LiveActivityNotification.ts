eafdceaca89a9c186fa1d93ced5a0b9911a2cc99
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-06-05 01:13:21 +0200

    Updated definitions of LA body and implementation

--- src/notifications/LiveActivityNotification.ts
+++ src/notifications/LiveActivityNotification.ts
@@ -17,6 +17,52 @@ type LiveActivityNotificationBody = {
 	 * For more information, see [Displaying live data with Live Activities](https://developer.apple.com/documentation/ActivityKit/displaying-live-data-with-live-activities).
 	 */
 	staleDate?: number;
+
+	/**
+	 * A number that the system uses to sort the
+	 * notifications from your app.
+	 *
+	 * The highest score gets featured in the notification summary.
+	 * See relevanceScore.
+	 *
+	 * If your remote notification updates a Live Activity, you can
+	 * set any `Double` value; for example, 25, 50, 75, or 100.
+	 */
+
+	relevanceScore?: number;
+
+	/**
+	 * The UNIX timestamp that marks the time when you send the remote
+	 * notification that updates or ends a Live Activity.
+	 *
+	 * For more information, see `Updating and ending your Live Activity
+	 * with ActivityKit push notifications`.
+	 *
+	 * ---
+	 *
+	 * Implementation note: this is optional: if not provided, it will
+	 * fallback to `Date.now()`. This could be useful if you are sending
+	 * frequent updates and might not be able to ensure an order.
+	 *
+	 * Please also note that Push Notification console requires it
+	 * when sending a start event, so this library allows it for all
+	 * the kinds.
+	 */
+	timestamp?: number;
+
+	/**
+	 * The updated or final content for a Live Activity. The content of
+	 * this dictionary must match the data you describe with your custom
+	 * [ActivityAttributes](https://developer.apple.com/documentation/ActivityKit/ActivityAttributes) implementation.
+	 *
+	 * For more information, see `Updating and ending your Live Activity
+	 * with ActivityKit push notifications`.
+	 *
+	 * ---
+	 *
+	 * Implementation note:
+	 */
+	contentState: Record<string, unknown>;
 } & (
 	| {
 			/**
@@ -30,32 +76,8 @@ type LiveActivityNotificationBody = {
 			 * For more information, see `Updating and ending your Live Activity
 			 * with ActivityKit push notifications`.
 			 */
-			event: "update" | "end";
 
-			/**
-			 * The updated or final content for a Live Activity. The content of
-			 * this dictionary must match the data you describe with your custom
-			 * [ActivityAttributes](https://developer.apple.com/documentation/ActivityKit/ActivityAttributes) implementation.
-			 *
-			 * For more information, see `Updating and ending your Live Activity
-			 * with ActivityKit push notifications`.
-			 */
-			contentState: Record<string, unknown>;
-
-			/**
-			 * The UNIX timestamp that marks the time when you send the remote
-			 * notification that updates or ends a Live Activity.
-			 *
-			 * For more information, see `Updating and ending your Live Activity
-			 * with ActivityKit push notifications`.
-			 *
-			 * ---
-			 *
-			 * Implementation note: this is optional: if not provided, it will
-			 * fallback to `Date.now()`. This could be useful if you are sending
-			 * frequent updates and might not be able to ensure an order.
-			 */
-			timestamp?: number;
+			event: "end";
 
 			/**
 			 * The UNIX timestamp that represents the date at which the system
@@ -64,8 +86,51 @@ type LiveActivityNotificationBody = {
 			 *
 			 * For more information, see `Updating and ending
 			 * your Live Activity with ActivityKit push notifications`.
+			 *
+			 * Default: 4 hours (system default).
+			 *
+			 * @see https://developer.apple.com/documentation/activitykit/starting-and-updating-live-activities-with-activitykit-push-notifications#End-the-Live-Activity-with-a-custom-dismissal-date
 			 */
 			dismissalDate?: number;
+
+			/**
+			 * Alert will light up the device in case of critical notifications and play
+			 * a sound, if provided.
+			 *
+			 * Title and body are shown as a traditional alert notification on the devices
+			 * that do not support Live Activities. Both are mandatory for a notification
+			 * to run.
+			 */
+			alert?: {
+				title: AlertField;
+				body: AlertField;
+
+				/**
+				 * The exact filename of the sound file to play when the
+				 * notification is received.
+				 *
+				 * Please note that, for Live Activities, you cannot
+				 * specify other details (just like classic Alert notifications).
+				 *
+				 * If not specified, the system will play the default sound
+				 * (like you specified "default" as value).
+				 */
+				sound?: string;
+			};
+	  }
+	| {
+			/**
+			 * The string that describes whether you start, update, or end an
+			 * ongoing Live Activity with the remote push notification.
+			 *
+			 * To start the Live Activity, use `start`.
+			 * To update the Live Activity, use `update`.
+			 * To end the Live Activity, use `end`.
+			 *
+			 * For more information, see `Updating and ending your Live Activity
+			 * with ActivityKit push notifications`.
+			 */
+			event: "update";
 
 			/**
 			 * Alert will light up the device in case of critical notifications and play
@@ -173,7 +238,7 @@ export function LiveActivityNotification(
 	}
 
 	const { expiration = 0, collapseID, priority = 10, payload } = data;
-	const { event, staleDate } = payload;
+	const { event, staleDate, timestamp = Date.now(), relevanceScore, contentState, alert } = payload;
 
 	return {
 		pushType: "liveactivity",
@@ -193,6 +258,10 @@ export function LiveActivityNotification(
 						event,
 						"stale-date": staleDate,
 						"attributes-type": attributesType,
+						attributes,
+						"relevance-score": relevanceScore,
+						"content-state": contentState,
+						timestamp,
 						alert: {
 							title: payload.alert?.title,
 							body: payload.alert?.body,
@@ -202,15 +271,31 @@ export function LiveActivityNotification(
 				};
 			}
 
-			const { contentState, timestamp = Date.now(), dismissalDate, alert } = payload;
+			if (event === "end") {
+				const { dismissalDate } = payload;
+
+				const notificationBody = {
+					aps: {
+						event,
+						"stale-date": staleDate,
+						"content-state": contentState,
+						timestamp,
+						"relevance-score": relevanceScore,
+						"dismissal-date": dismissalDate,
+						alert: createNotificationAlertBody(alert),
+					} satisfies Notification<LiveActivityNotificationBody>["body"]["aps"],
+				};
+
+				return notificationBody;
+			}
 
 			const notificationBody = {
 				aps: {
 					event,
 					"stale-date": staleDate,
 					"content-state": contentState,
+					"relevance-score": relevanceScore,
 					timestamp,
-					"dismissal-date": dismissalDate,
 					alert: createNotificationAlertBody(alert),
 				} satisfies Notification<LiveActivityNotificationBody>["body"]["aps"],
 			};
