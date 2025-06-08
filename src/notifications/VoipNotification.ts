e4d3f30bc950cadcb92d12dac3f9176e8aab364f
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-06-08 13:18:22 +0200

    Added expiration time artificial clamping in VoipNotification as per Apple Documentation

--- src/notifications/VoipNotification.ts
+++ src/notifications/VoipNotification.ts
@@ -1,5 +1,7 @@
 import { assertTopicProvided } from "../errors/assertions/topic-provided.js";
+import { createError } from "../errors/create.js";
 import type { Notification, NotificationBody, NotificationHeaders } from "./notification.js";
+
 /**
  * Empty interface on purpose to allow for TS
  * autocompletion to be extended by the user.
@@ -7,13 +9,31 @@ import type { Notification, NotificationBody, NotificationHeaders } from "./noti
  */
 export interface NotificationCustomData {}
 
-type NotificationData = NotificationHeaders &
-	NotificationBody<Record<string, string>, NotificationCustomData>;
+type NotificationData = NotificationHeaders & NotificationBody<never, NotificationCustomData>;
+
+const EXPIRATION_TOO_BIG_ERROR = createError(
+	"EXPIRATION_TOO_BIG",
+	"Cannot create notification: expiration time for VoIP notifications must be included between 0 and 5 seconds. This artificial limit reflects a requirement from Apple. Received: %s",
+);
+
+const EXPIRATION_NAN_ERROR = createError(
+	"EXPIRATION_NAN",
+	"Cannot create notification: expiration time for VoIP notifications must be a number. Received: %s",
+);
 
 const TOPIC_SUFFIX = ".voip";
 
 /**
  * Create a notification about an incoming VoIP call through PushKit.
+ *
+ * `expiration` field is clamped to be lower than 5 seconds as per Apple requirement:
+ *
+ * ```text
+ * Set the value of the apns-expiration header field to 0, or to only a few seconds.
+ * Doing so prevents the system from delivering the notification at a much later time.
+ * ```
+ *
+ * @see https://developer.apple.com/documentation/PushKit/responding-to-voip-notifications-from-pushkit#Generate-Push-Notifications-from-Your-Server
  *
  * @param appBundleId The topic of the notification. It will be suffixed, if needed, with `.voip`.
  * @param data
@@ -26,6 +46,8 @@ export function VoipNotification(
 	assertTopicProvided(appBundleId);
 
 	const { expiration = 0, collapseID, priority = 10 } = data;
+
+	assertExpirationValid(expiration);
 
 	return {
 		pushType: "voip",
@@ -43,4 +65,18 @@ export function VoipNotification(
 		collapseID,
 		priority,
 	};
+}
+
+function assertExpirationValid(expiration: unknown): asserts expiration is number {
+	if (typeof expiration === "undefined") {
+		return;
+	}
+
+	if (typeof expiration !== "number") {
+		throw new EXPIRATION_NAN_ERROR(expiration);
+	}
+
+	if (expiration < 0 || expiration > 5) {
+		throw new EXPIRATION_TOO_BIG_ERROR(expiration);
+	}
 }
