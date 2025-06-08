f9a32eea35ad0a4a9ca44b1076d096d234becaa6
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-06-08 18:38:00 +0200

    Uniformed errors in send function

--- src/send/index.ts
+++ src/send/index.ts
@@ -1,6 +1,27 @@
 import type { APNsHeaders, ConnectorProtocol, DeliveryResult } from "../connectors/connector.js";
+import { createError } from "../errors/create.js";
 import type { Notification } from "../notifications/notification.js";
 import type { NotificationTarget } from "../targets/target.js";
+
+const CONNECTOR_INVALID_ERROR = createError(
+	"CONNECTOR_INVALID_ERROR",
+	"Cannot send notification: Connector is missing or is not a valid connector.",
+);
+
+const NOTIFICATION_INVALID_ERROR = createError(
+	"NOTIFICATION_INVALID_ERROR",
+	"Cannot send notification: Notification is missing or is not a valid notification.",
+);
+
+const TOPIC_PUSH_TYPE_INVALID_ERROR = createError(
+	"TOPIC_PUSH_TYPE_INVALID_ERROR",
+	"Cannot send notification: Provided notification object is not a valid notification as is missing topic or push type.",
+);
+
+const TARGET_INVALID_ERROR = createError(
+	"TARGET_INVALID_ERROR",
+	"Cannot send notification: Target is missing or is not a valid target.",
+);
 
 export async function send(
 	connector: ConnectorProtocol,
@@ -9,21 +30,19 @@ export async function send(
 	useSandbox: boolean = false,
 ): Promise<DeliveryResult> {
 	if (!connector || typeof connector.send !== "function") {
-		throw new Error("Connector is missing or is not a valid connector.");
+		throw new CONNECTOR_INVALID_ERROR();
 	}
 
 	if (!notification || typeof notification !== "object") {
-		throw new Error("Notification is missing or is not a valid notification.");
+		throw new NOTIFICATION_INVALID_ERROR();
 	}
 
 	if (!notification.topic || !notification.pushType) {
-		throw new Error(
-			"Provided notification is not a valid notification as is missing topic or push type.",
-		);
+		throw new TOPIC_PUSH_TYPE_INVALID_ERROR();
 	}
 
 	if (!target || typeof target !== "object") {
-		throw new Error("Target is missing or is not a valid target.");
+		throw new TARGET_INVALID_ERROR();
 	}
 
 	const headers = {
