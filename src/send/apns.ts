ab6a07ae165c0a05eb6e68be74df854e41819052
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-08-11 10:52:36 +0200

    Added apns function to export the file ready for simulator

---
+++ src/send/apns.ts
@@ -0,0 +1,32 @@
+import type { APSBody, Notification } from "../notifications/notification.js";
+import type { NotificationTarget } from "../targets/target.js";
+
+type APSObject = APSBody<Record<string, string>>;
+
+interface ApnsFileContent extends APSObject {
+	"Simulator Target Bundle": string;
+}
+
+/**
+ * This function generates the content of an .apns file
+ * ready for the Simulator. The only step missing is writing
+ * on a file to be either pushed or dragged.
+ *
+ * Although, this seems to be working only for alert notifications.
+ *
+ * @see https://developer.apple.com/documentation/watchos-apps/testing-custom-notification-interfaces#Create-a-payload-file
+ *
+ * Please note that, starting Xcode 14, Simulator supports
+ * notifications through APNS in the sandbox environment.
+ *
+ * @see https://developer.apple.com/documentation/xcode-release-notes/xcode-14-release-notes#Simulator
+ */
+
+export function apns<N extends Notification<APSObject>>(notification: N): string {
+	const body: ApnsFileContent = {
+		...notification.body,
+		"Simulator Target Bundle": notification.topic,
+	};
+
+	return JSON.stringify(body);
+}
